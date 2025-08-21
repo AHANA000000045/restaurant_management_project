4e6a4de02b55ca408c96fd25f876092fe17a91ae
AHANA000000045 <111188759+AHANA000000045@users.noreply.github.com>
2025-08-21 21:39:39 +0530

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,36 @@
+
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Order
+from .serializers import OrderSerializer
+from home.permissions import IsWaiter
+
+
+
+'''
+NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
+'''
 
 # Create your views here.
+class OrderView(APIView):
+    permission_classes=[IsAuthenticated]
+
+   
+
+    def post(self, request):
+        
+        if not IsWaiter().has_permission(request,self):
+            return Response({"detail":"Permission Denied"})
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Create your views here.
+
