4f488cb07dc2e482da10ceb052df7fbcf057c95b
AHANA000000045 <111188759+AHANA000000045@users.noreply.github.com>
2025-08-21 20:33:03 +0530

    Update models.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+class User(AbstractUser):
+ROLE_CHOICES=[
+('Admin','Admin'),
+  ('Manager','Manager'),
+  ('Cashier','Cashier'),
+  ('waiter','Waiter'),
 
+  
+]
+
+
+role=models.CharDField(max_length=20,choices=ROLE_CHOICES,default='Waiter')
+def__str__(self):
+return f"{self.username}({self.role})"
 # Create your models here.
+
