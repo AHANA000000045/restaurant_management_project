93bb56f9d9c5592e88f5104305ee5ac03af16dc0
AHANA000000045 <111188759+AHANA000000045@users.noreply.github.com>
2025-08-21 20:43:44 +0530

    Create permissions.py

---
+++ orders/permissions.py
@@ -0,0 +1,5 @@
+from rest_framework.permissions import BasePermission
+class IsAdminOrManager(BasePermission):
+  def has_Permission(self,request,view):
+    return request.user.is_authenticated and request.user.role =='Waiter'
+    
