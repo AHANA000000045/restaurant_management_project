eee595e75e2e1dae74ffd6fe55e13755b020e918
AHANA000000045 <111188759+AHANA000000045@users.noreply.github.com>
2025-08-21 21:34:47 +0530

    Update views.py

--- products/views.py
+++ products/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Item
 from .serializers import ItemSerializer
+from home.permissions import IsManagerOrAdmin
 
-class IsManageroradmin:
-    def has__permission(self,request,view):
-        return request.user.is_authenticated and request.user.role in['Manager','Admin']
+
 
 '''
 NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
@@ -24,9 +24,13 @@ class ItemView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
+        self.check_permissions(request)
+        if not IsManagerOrAdmin().has_permission(request,self):
+            return Response({"detail":"Permission Denied"})
         serializer = ItemSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
